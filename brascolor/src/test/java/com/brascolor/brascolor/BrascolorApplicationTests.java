ea23183740ad41cff407fbfd10ddf133c9bd4c9b
C4BrrT <ccbc@cesar.school>
2023-11-05 18:18:42 -0300

    Added new Java project with Spring Boot

---
+++ brascolor/src/test/java/com/brascolor/brascolor/BrascolorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brascolor.brascolor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrascolorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
