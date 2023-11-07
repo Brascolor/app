8a55faa7194b9b14c7a042ef4f493635cfd37cd3
C4BrrT <ccbc@cesar.school>
2023-11-07 09:08:16 -0300

    Added new Thymeleaf, project working

--- brascolor/src/main/java/com/brascolor/brascolor/BrascolorController.java
+++ brascolor/src/main/java/com/brascolor/brascolor/BrascolorController.java
@@ -1,30 +1,16 @@
 package com.brascolor.brascolor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam; 
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;  
 
 @RestController
-@RequestMapping
 public class BrascolorController {
-    @Autowired
-    private UserRepository userRepository;
 
-    // Map ONLY POST Requests
-    @PostMapping(path = "/add")
-    public @ResponseBody User addNewUser(@RequestParam String name
-            , @RequestParam String email) {
-        // @ResponseBody means the returned User is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-        User springUser = new User();
-        springUser.setName(name);
-        springUser.setEmail(email);
-        userRepository.save(springUser);
-        return springUser;
-    }
-
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<User> getAllUsers() { 
-        // This returns a JSON or XML with the users
-        return userRepository.findAll();
+    @GetMapping("/")
+    public String home(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 }
