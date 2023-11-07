f42047169979386c42d23cf5218ed29105b5357e
C4BrrT <ccbc@cesar.school>
2023-11-07 09:35:00 -0300

    Adapting Project

--- brascolor/src/main/java/com/brascolor/brascolor/BrascolorController.java
+++ brascolor/src/main/java/com/brascolor/brascolor/BrascolorController.java
@@ -2,15 +2,20 @@ package com.brascolor.brascolor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam; 
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;  
 
 @RestController
-public class BrascolorController {
-
+public class BrascolorController { 
+  
     @GetMapping("/")
-    public String home(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @RequestMapping(
+        method = {RequestMethod.GET},
+        value = {"/home"})
+    @ResponseBody 
+    public String home() { 
+        return "home";
     }
 }
