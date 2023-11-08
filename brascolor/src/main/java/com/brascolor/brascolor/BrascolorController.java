f1161aabdf862be6ddfe66027df38a48724198dc
C4BrrT <ccbc@cesar.school>
2023-11-08 08:59:05 -0300

    Added login pt1, added wrong logical

--- brascolor/src/main/java/com/brascolor/brascolor/BrascolorController.java
+++ brascolor/src/main/java/com/brascolor/brascolor/BrascolorController.java
@@ -1,17 +1,34 @@
 package com.brascolor.brascolor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam; 
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Controller
-public class BrascolorController { 
-  
+public class BrascolorController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    
+    @GetMapping("/login")
+    public String login(@ModelAttribute("user") User user){
+        if(user.isLoggedIn()){
+            return "redirect:/home"; 
+        }
+            return "components/login";
+    }
+
     @GetMapping("/")
     public String home() { 
-        return "components/home.html";
+        return "components/home";
     }
 }
---
+++ brascolor/src/main/java/com/brascolor/brascolor/User.java
@@ -0,0 +1,13 @@
+package com.brascolor.brascolor;
+
+public class User {
+    private boolean isLoggedIn;
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
+}
